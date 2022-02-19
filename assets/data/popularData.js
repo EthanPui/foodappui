2d6c024942be76ec73d51a791f128ddb6a44b153
Ethan Pui <ethanpui@gmail.com>
2022-02-19 15:48:37 +0800

    Header, profile img and menu bar complete

---
+++ assets/data/popularData.js
@@ -0,0 +1,27 @@
+const popularData = [
+  {
+    id: 1,
+    title: 'Primavera Pizza',
+    image: require('../images/pizza1.png'),
+    rating: '5.0',
+    weight: '540 gr',
+  },
+
+  {
+    id: 2,
+    title: 'Vegetarian Pizza',
+    image: require('../images/pizza2.png'),
+    rating: '4.0',
+    weight: '450 gr',
+  },
+
+  {
+    id: 3,
+    title: 'Pepperoni Pizza',
+    image: require('../images/pizza3.png'),
+    rating: '5.0',
+    weight: '700 gr',
+  },
+];
+
+export default popularData;
---
+++ components/Home.js
@@ -0,0 +1,44 @@
+import {Text, StyleSheet, View, SafeAreaView, Image} from 'react-native';
+import * as React from 'react';
+import categoriesData from '../assets/data/categoriesData';
+import popularData from '../assets/data/popularData';
+import colors from '../assets/colors/colors';
+import Feather from 'react-native-vector-icons/Feather'
+
+Feather.loadFont();
+
+export default Home = () => {
+  return (
+    <View style={styles.container}>
+      <SafeAreaView>
+        <View style={styles.headerWrapper}>
+          <Image
+            source={require('../assets/images/profile.png')}
+            style={styles.profileImage}
+          />
+          <Feather name="menu" size={24} color={colors.textDark} />
+        </View>
+      </SafeAreaView>
+      <View></View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    alignItems: 'center',
+  },
+
+  profileImage: {
+    height: 40,
+    width: 40,
+    borderRadius: 40,
+  },
+});
