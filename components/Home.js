496c6114f32da85debea9c1e2a296ae79906627f
Ethan Pui <ethanpui@gmail.com>
2022-02-21 19:15:52 +0800

    Completed everything in popular section apart from image

--- components/Home.js
+++ components/Home.js
@@ -11,6 +11,7 @@ import categoriesData from '../assets/data/categoriesData';
 import popularData from '../assets/data/popularData';
 import colors from '../assets/colors/colors';
 import Feather from 'react-native-vector-icons/Feather';
+import MatieralCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 Feather.loadFont();
 
@@ -82,6 +83,56 @@ export default Home = () => {
             horizontal={true}
           />
         </View>
+      </View>
+
+      {/* {Popular} */}
+      <View style={styles.popularWrapper}>
+        <Text style={styles.popularTitle}>Popular</Text>
+        {popularData.map(item => (
+          <View
+            style={[
+              styles.popularCardWrapper,
+              {
+                marginTop: item.id == 1 ? 10 : 20,
+              },
+            ]}>
+            <View>
+              <View>
+                <View style={styles.popularTopWrapper}>
+                  <MatieralCommunityIcons
+                    name="crown"
+                    size={12}
+                    color={colors.primary}
+                  />
+                  <Text style={styles.popularTopText}>top of the week</Text>
+                </View>
+                <View style={styles.popularTitlesWrapper}>
+                  <Text style={styles.popularTitlesTitle}>{item.title}</Text>
+                  <Text style={styles.popularTitlesWeight}>
+                    Weight {item.weight}
+                  </Text>
+                </View>
+              </View>
+
+              <View style={styles.popularCardBottom}>
+                <View style={styles.addPizzaButton}>
+                  <Feather name="plus" size={10} color={colors.textDark} />
+                </View>
+                <View style={styles.ratingWrapper}>
+                  <MatieralCommunityIcons
+                    name="star"
+                    size={10}
+                    color={colors.textDark}
+                  />
+                  <Text style={styles.rating}>{item.rating}</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.popularCardRight}>
+              <Image style={styles.popularCardImage} source={item.image} />
+            </View>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -196,5 +247,86 @@ const styles = StyleSheet.create({
 
   categorySelectIcon: {
     alignSelf: 'center',
+    fontSize: 15,
   },
+
+  popularWrapper: {
+    paddingTop: 20,
+    marginHorizontal: 20,
+  },
+  popularTitle: {
+    fontFamily: 'Montserrat-Bold',
+    color: colors.textDark,
+    fontSize: 16,
+  },
+
+  popularCardWrapper: {
+    backgroundColor: colors.white,
+    borderRadius: 25,
+    paddingTop: 20,
+    paddingLeft: 20,
+    flexDirection: 'row',
+  },
+
+  popularTopWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  popularTopText: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 14,
+    marginLeft: 10,
+    color: colors.textDark,
+  },
+
+  popularTitlesWrapper: {
+    marginTop: 20,
+    marginLeft: 22,
+  },
+  popularTitlesTitle: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 14,
+    color: colors.textDark,
+  },
+  popularTitlesWeight: {
+    fontFamily: 'Montserrat-Medium',
+    fontSize: 12,
+    color: colors.textLight,
+    marginTop: 5,
+  },
+
+  popularCardBottom: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+    marginLeft: -20,
+  },
+
+  addPizzaButton: {
+    backgroundColor: colors.primary,
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    borderTopRightRadius: 25,
+    borderBottomLeftRadius: 25,
+  },
+  ratingWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 20,
+  },
+  rating: {
+    marginLeft: 5,
+    fontSize: 12,
+    fontFamily: 'Montserrat-SemiBold',
+    color: colors.textDark,
+  },
+
+  popularCardRight: {
+  },
+  popularCardImage: {
+    width: 210,
+    height: 125,
+    resizeMode: 'contain',
+  },  
 });
